79633923cc63c984a659abd3a4adcee7bb1013f8
codingbanana <bithao8486@gmail.com>
2015-11-11 14:52:19 -0500

    rewrite code

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,21 @@
 ##Coursera: "Getting and Cleaning Data" course project
-# Hao Zhang @ 2015.10.13
+# Hao Zhang @ 2015.11.10
 #create a function to include or install required packages
-req_pkg <- function(pkg){
-    if(pkg %in% installed.packages()){
-        library(pkg, character.only=TRUE)
-    }else{
-        install.packages(pkg)
+include <- function(pkg) {
+    for (pk in pkg){
+        if(!(pk %in% installed.packages())) {
+            install.packages(pk)
+        }
+        library(pk,character.only = T)
     }
 }
 #load dplyr and reshape2 packages
-req_pkg("dplyr")
-req_pkg("reshape2")
+include(c("dplyr","reshape2"))
 
 ##download the zip file if it hasn't been downloaded
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 if (!file.exists("project.zip")){
-    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","project.zip",method = "curl")
+    download.file(URL,"project.zip")    #windows doesn't like method='curl'
 }
 ##unzip file if it hasn't been unziped
 if (!dir.exists("project")){
@@ -27,17 +28,12 @@ X_train <- read.table(file = "project/X_train.txt",header = F)
 X_test <- read.table(file = "project/X_test.txt",header = F)
 # X is the data set of test + train
 X <- rbind(X_test,X_train)
-
 # add colnames from features.txt
 feature <- read.table(file="project/features.txt",header=F,stringsAsFactors = F)
-feature <- feature[,2]
-colnames(X) <- feature
+colnames(X) <- feature[,2]
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-sub <- subset(X,select=grepl("mean",names(X)))
-sub2 <- subset(X,select=grepl("std",names(X)))
-#combines the two data frames
-sub <- cbind(sub,sub2)
+sub <- subset(X,select=grepl("mean|std",names(X)))
 
 # 3. Uses descriptive activity names to name the activities in the data set
 y_test <- read.table(file="project/y_test.txt",header = F)
@@ -48,32 +44,12 @@ y  <- rbind(y_test,y_train)
 # act is the activities from activitity_labels.txt
 act <- read.table("project/activity_labels.txt",header = F,col.names = c("activity","activity_name"))
 #left join the activity index and the descriptive activities name
-y_act <- merge(x=y,y=act,by.x=1,by.y=1, all.x=TRUE)
+y_act <- merge(x=y,y=act,by.x=1,by.y="activity", all.x=TRUE)
 # xy is the tidy data that includes data and descriptive activities for test + train
 xy <- cbind(sub,y_act[2])
 write.table(xy,"xy.txt",row.names = F)
 # 4. Appropriately labels the data set with descriptive variable names.
-xy_col <- colnames(xy)
-#replace colnames with descriptive variable names
-temp <- gsub("Acc","Acceleration", xy_col)
-temp <- gsub("gyro","AngularVelocity", temp)
-temp <-gsub("Mag","Magnitude", temp)
-temp <-gsub("-mean()","Mean",temp)
-temp <-gsub("-std()","StandardDeviation",temp)
-temp <-gsub("-X","Xaxis",temp)
-temp <-gsub("-Y","Yaxis",temp)
-temp <-gsub("-Z","Zaxis",temp)
-# replace the first character from either t or f to 'time' or 'frequency'
-for (i in 1:length(temp)) {
-    if (substr(temp[i],1,1)=="t") {
-        temp[i] <- paste("time",substring(temp[i],2),sep = "")
-    }
-    if (substr(temp[i],1,1)=="f") {
-        temp[i] <- paste("frequency",substring(temp[i],2),sep="")
-    }
-}
-# reassign colnames to xy (tidy data)
-colnames(xy) <- temp
+#done in step 1
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 sub_test <- read.table("project/subject_test.txt",header = F)
@@ -83,6 +59,8 @@ sub <- rbind(sub_test,sub_train)
 colnames(sub) <- "subject"
 # xy_sub is the combined data frame of xy + subject
 xy_sub <- cbind(xy,sub)
+#agg <- lapply(xy_sub,function(x){tapply(x,xy_sub[,81:80], mean)})
+
 # agg is the summarized data set that has the averaged values
 agg <- aggregate(xy_sub[,1:79],by=list(xy_sub$subject,xy_sub$activity_name),mean)
 #rename the first two columns
